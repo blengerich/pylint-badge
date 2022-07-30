27d11f96bdfc8e0726ff190247f10156ca745be5
Ben Lengerich <blengerich@gmail.com>
2022-07-30 12:47:23 -0400

    fix the issue with pylint versions

--- README.md
+++ README.md
@@ -1,19 +1,41 @@
-# pybadge
+# pylint-badge
 
-A simple and functional pylint badge generator. Better to use [anybadge](https://github.com/jongracecox/anybadge) for most purposes.
+![pylint Score](pylint.svg)
 
-## Howto
+A simple and functional `pylint` badge generator.
 
-1. Install via `pip` or other method.
-2. Run as follows: `pylint-badge script.py pylint.svg` which will produce a file `pylint.svg` in the current directory. 
+This version makes a minor fix of [pylint-badge](https://github.com/rossi-fi/pylint-badge) to support multiple versions of `pylint`.
+
+Better to use [anybadge](https://github.com/jongracecox/anybadge) for most purposes.
+
+
+## How to Use
+
+1. Install via `pip` or other method:
+`pip install git+https://github.com/blengerich/pylint-badge`
+
+2. Run as follows: `pylint-badge script.py pylint.svg` which will produce a file `pylint.svg` in the current directory.
+
+3. Add to your README.md:
+`![pylint Score](pylint.svg)`
 
 ## Color intervals
 
+By Default:
 0.00 < ![pylint Score](https://mperlet.github.io/pybadge/badges/1.50.svg) < 3.00 < ![pylint Score](https://mperlet.github.io/pybadge/badges/5.51.svg) < 7.00 ![pylint Score](https://mperlet.github.io/pybadge/badges/9.73.svg) < 10.00
+
+The thresholds for colors can be changed by the args:
+`
+--min_yellow
+--min_green
+`
+which set the minimum threshold of `pylint` score for the yellow or green colors to be assigned.
 
 ## Limitations
 
 * Negative scores are not supported
 
+
 ## Idea
-Inspired by mperlet's [pybadge](https://github.com/mperlet/pybadge), which was in turn inspired by [this blogpost](http://www.desmondrivet.com/blog/technical/pylint-badges.html.)
+
+Forked from [pylint-badge](https://github.com/rossi-fi/pylint-badge), which was "Inspired by mperlet's [pybadge](https://github.com/mperlet/pybadge), which was in turn inspired by [this blogpost](http://www.desmondrivet.com/blog/technical/pylint-badges.html.)"
--- pylintbadge/pylintbadge.py
+++ pylintbadge/pylintbadge.py
@@ -4,17 +4,33 @@ import argparse
 from pylint.lint import Run
 
 
+DEFAULT_MIN_YELLOW = 3.0
+DEFAULT_MIN_GREEN = 7.0
+
+
 def main():
     """Main deal"""
     parser = argparse.ArgumentParser()
     parser.add_argument("file_to_lint")
     parser.add_argument("output_file")
+    parser.add_argument("--min_yellow", type=float)
+    parser.add_argument("--min_green", type=float)
     args = parser.parse_args()
     file_to_lint = args.file_to_lint
+    min_yellow = args.min_yellow
+    if min_yellow is None:
+        min_yellow = DEFAULT_MIN_YELLOW
+    min_green = args.min_green
+    if min_green is None:
+        min_green = DEFAULT_MIN_GREEN
 
-    score = round(Run([file_to_lint], do_exit=False).linter.stats['global_note'], 2)
-
-    template = '''
+    linterstats_obj = Run([file_to_lint], do_exit=False).linter.stats
+    try:
+        score = linterstats_obj["global_note"]
+    except TypeError:
+        score = linterstats_obj.global_note
+    score = round(score, 2)
+    template = """
 <svg xmlns="http://www.w3.org/2000/svg" width="85" height="20">
     <linearGradient id="a" x2="0" y2="100%">
         <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
@@ -27,20 +43,24 @@ def main():
         <text x="25" y="15" fill="#010101" fill-opacity=".3">pylint</text><text x="25" y="14">pylint</text>
         <text x="67" y="15" fill="#010101" fill-opacity=".3">{score}</text><text x="67" y="14">{score}</text>
     </g>
-</svg>'''.replace('\n', '').replace('    ', '')
+</svg>""".replace(
+        "\n", ""
+    ).replace(
+        "    ", ""
+    )
 
     color_ok = "#44cc11"
     color_warning = "#dfb317"
     color_red = "#e05d44"
 
-    if float(score) < 3.0:
+    if float(score) < min_yellow:
         color = color_red
-    elif 3.0 <= float(score) < 7.0:
+    elif min_yellow <= float(score) < min_green:
         color = color_warning
     else:
         color = color_ok
 
-    with open(args.output_file, 'w') as score_file:
+    with open(args.output_file, "w") as score_file:
         score_file.write(template.format(score=score, color=color))
 
 
